2dfa5320ea70567455bea061ad63f2c0a2add4b3
Anton Zarichny <antonzarichny@gmail.com>
2022-08-23 17:46:10 +0300

    Some logic with TF's

--- ColorizedApp/ViewControllers/SettingsViewController.swift
+++ ColorizedApp/ViewControllers/SettingsViewController.swift
@@ -18,13 +18,26 @@ final class SettingsViewController: UIViewController {
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redValueTF: UITextField!
+    @IBOutlet var greenValueTF: UITextField!
+    @IBOutlet var blueValueTF: UITextField!
+    
     // MARK: - Properties
     var viewColor: UIColor!
     
     var delegate: SettingsViewControllerDelegate!
     // MARK: - Override functions
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        redValueTF.delegate = self
+        greenValueTF.delegate = self
+        blueValueTF.delegate = self
         
         decomposeRGB()
         
@@ -33,18 +46,21 @@ final class SettingsViewController: UIViewController {
     // MARK: - IBActions
     @IBAction func redSliderAction() {
         redValue.text = String(format: "%.2f", redSlider.value)
+        redValueTF.text = String(format: "%.2f", redSlider.value)
         
         setupColorMainView()
     }
     
     @IBAction func greenSliderAction() {
         greenValue.text = String(format: "%.2f", greenSlider.value)
+        greenValueTF.text = String(format: "%.2f", greenSlider.value)
 
         setupColorMainView()
     }
     
     @IBAction func blueSliderAction() {
         blueValue.text = String(format: "%.2f", blueSlider.value)
+        blueValueTF.text = String(format: "%.2f", blueSlider.value)
        
         setupColorMainView()
     }
@@ -76,6 +92,35 @@ final class SettingsViewController: UIViewController {
         redValue.text = String(format: "%.2f", redSlider.value)
         greenValue.text = String(format: "%.2f", greenSlider.value)
         blueValue.text = String(format: "%.2f", blueSlider.value)
+        
+        redValueTF.text = String(format: "%.2f", redSlider.value)
+        greenValueTF.text = String(format: "%.2f", greenSlider.value)
+        blueValueTF.text = String(format: "%.2f", blueSlider.value)
+        
+    }
+}
+
+extension SettingsViewController {
+    private func showAlert(with title: String, and massage: String) {
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
+extension SettingsViewController: UITextFieldDelegate{
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        switch textField {
+        case redValueTF:
+            redValue.text = redValueTF.text
+        case greenValueTF:
+            greenValue.text = greenValueTF.text
+        default:
+            blueValue.text = blueValueTF.text
+        }
     }
 }
 
